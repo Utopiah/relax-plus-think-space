a479cb8abc843e0b4cced93a28bb35639c13f519
Fabien Benetou <fabien-services@benetou.fr>
2019-12-24 17:35:38 +0100

    start

---
+++ setup/upload/server/php/files/usersdb.js
@@ -0,0 +1,18 @@
+var database = {
+	"fabien@benetou.fr" : {
+		"username": "fabien",
+		"password": "test",
+		"last_login": 1576749381516,
+		"environment": "forest",
+		"files" : [
+			{ "position": "-2 1 -2", "filename" : "upload/server/php/files/C1C4E2F7-865A-4DB4-9274-1EA69B6C4A7E.jpeg" },
+			{ "position": "0 1 -2", "filename" : "upload/server/php/files/4A21A76C-F375-4884-9040-BC51A24057A6.jpeg" },
+			{ "position": "2 1 -2", "filename" : "upload/server/php/files/2CD9932F-891C-4BA5-BDDA-A3B39B39CF09.jpeg" }
+		],
+		"categories" : [
+			{ "position": "-2 1 -2", "color" : "orange", "label" : "waiting" },
+			{ "position": "0 1 -2", "color" : "red", "label" : "in progress" },
+			{ "position": "2 1 -2", "color" : "green", "label" : "completed" }
+		]
+	 }
+}
